f79ac50657690f9e07329387cb605a629f61340a
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2020-05-22 11:46:31 -0400

    imported the BubblePage.js file into the App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./components/Login";
 import "./styles.scss";
 import PrivateRoute from './components/PrivateRoute';
+import BubblePage from './components/BubblePage';
 
 function App() {
   return (
